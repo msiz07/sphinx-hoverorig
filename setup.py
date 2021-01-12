7bba839ab603ece33ca5e155ac9110efe61165a1
msiz07 <55676424+msiz07@users.noreply.github.com>
2021-01-12 23:12:46 +0900

    reorganize dir structure according to setuptools doc

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_namespace_packages
+
+# Since version 30.3.0, Setuptools allows using configuration files (usually
+# setup.cfg) to define a package's metadata and other options that are
+# normally supplied to the setup() function (declarative config).
+# see, https://setuptools.readthedocs.io/en/latest/userguide/declarative_config.html
+setup(
+    #packages=find_namespace_packages(where="src"),
+)
